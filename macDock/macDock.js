fcb9c90e2c58a8be13a951fcba1485ea7cac7aab
Sudeer Kamat <sudeerkamat@gmail.com>
2022-12-28 22:52:49 +0530

    Add files via upload

---
+++ macDock/macDock.js
@@ -0,0 +1,92 @@
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from "lightning/navigation";
+
+export default class MacDock extends LightningElement {
+
+salesApp ='';
+servicesApp='';
+marketingApp='';
+salesConsoleApp='';
+serviceConsoleApp='';
+communityApp='';
+OtherApp='';
+codeBuiler='';
+
+orgURL='';
+
+    sales(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: orgURL + salesApp
+            }
+          });
+         }
+
+
+       services(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: orgURL + servicesApp
+            }
+          });
+       }  
+
+       market(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: orgURL + marketingApp
+            }
+          });
+       }  
+
+       salesConsole(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: orgURL + salesConsoleApp
+            }
+          });
+       }  
+
+       
+       serviceConsole(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: orgURL + serviceConsoleApp
+            }
+          });
+       }  
+
+              
+       community(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: orgURL + communityApp
+            }
+          });
+       }
+
+       other(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: orgURL + OtherApp
+            }
+          });
+       }
+
+       
+       codeBuiler(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: orgURL + codeBuiler
+            }
+          });
+       }
+}
